5a561e38ee40e5a103a0f2b9097fa56568d9675c
jp <jp@92f0537e-14c0-1d44-b2ba-3b60a4d6dcee>
2009-02-19 18:25:07 +0000

    Test sln

---
+++ managed/Cfix.Addin/testsolutions/VCProjectModuleLoading/WrongProjectType/Dummy.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WrongProjectType
+{
+	public class Dummy
+	{
+		public static void Main( string[] args )
+		{ }
+	}
+}
