ac2b1d0ec63a274539ea642eed737bbebce0a949
(no author) <(no author)@92f0537e-14c0-1d44-b2ba-3b60a4d6dcee>
2010-01-17 17:35:18 +0000

    New gray style

--- doc/joomla-template/index.php
+++ doc/joomla-template/index.php
@@ -7,25 +7,59 @@ defined( '_JEXEC' ) or die( 'Restricted access' );
 	<head>
 		<jdoc:include type="head" />
 		
-		<link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/cfixstudio/assets/master.css" type="text/css">
+		<link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/visualassert/assets/master.css" type="text/css">
 		<!--[if lt IE 7]>
-		<link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/cfixstudio/assets/master-ie6patch.css" type="text/css">
+		<link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/visualassert/assets/master-ie6patch.css" type="text/css">
 		<style>
 			#main_content { padding: 10px; }
 		</style>
 		<![endif]-->
+	
+		<style>
+			.ticks ul 
+			{
+				list-style-type: none;
+				padding: 0em;
+				margin: 0em;
+			}
+
+			.ticks ul li 
+			{
+				background-image: url(/unit-testing-framework/images/stories/tick.png);
+				background-repeat: no-repeat;
+				background-position: 0em .4em;
+				padding-left: 25px;
+				padding-bottom: 10px;
+			}
+
+			ul.newsfeed 
+			{
+				list-style-type: none;
+				padding-left: 0px;
+			}
+
+			.newsfeed_item
+			{
+				padding-top: 10px;
+				padding-bottom: 10px;
+				padding-left: 10px;
+			}
+		</style>
+
 	</head>
 	<body>
 		<div id='tab'>
-			<div class='tab_active'><a href='http://www.cfix-studio.org/'>cfix studio &#x2013; C/C++ unit testing for Visual Studio</a></div>
-			<div class='tab_active2passive'><img src='<?php echo $this->baseurl ?>/templates/cfixstudio/assets/img/tab_active2passive.png' alt=''/></div>
+			<div class='tab_activebegin'><img src='<?php echo $this->baseurl ?>/templates/visualassert/assets/img/tab_activebegin.png' alt=''/></div>
+			<div class='tab_active'><a href='http://www.visualassert.com/'>Visual Assert &#x2013; The Unit Testing Add-In for Visual C++</a></div>	
+			<div class='tab_active2passive'><img src='<?php echo $this->baseurl ?>/templates/visualassert/assets/img/tab_active2passive.png' alt=''/></div>
 			<div class='tab_passive'><a href='http://www.cfix-testing.org/'>cfix &#x2013;  C/C++ unit testing for Win32 and NT</a></div>
-			<div class='tab_passiveend'><img src='<?php echo $this->baseurl ?>/templates/cfixstudio/assets/img/tab_passiveend.png' alt=''/></div>
+			<div class='tab_passiveend'><img src='<?php echo $this->baseurl ?>/templates/visualassert/assets/img/tab_passiveend.png' alt=''/></div>
 			<div class='tab_pad'>&#xA0;</div>
 			<div class='tab_clear'></div>
 	    </div>
+		<div id='topborder'>&#xA0;</div>
 		<div id='header'>
-			<img src='<?php echo $this->baseurl ?>/templates/cfixstudio/assets/img/logo-cfixstudio.gif' alt='cfix studio &#x2013; C/C++ unit testing for Visual Studio' style="margin-left: 10px"/>
+			<img src='<?php echo $this->baseurl ?>/templates/visualassert/assets/img/logo-visualassert.gif' alt='Visual Assert &#x2013; The Unit Testing Add-In for Visual C++' style="margin-left: 30px"/>
 	    </div>
 	    <div id='menu'>
 	        <div id='menu_box'>
@@ -33,25 +67,38 @@ defined( '_JEXEC' ) or die( 'Restricted access' );
 		    </div>
 	    </div>
 		<div id='main'>
-	        <div id='main_sidebar'>
-				<div class='submenu'>
-				    <div class='submenu_box'>
-					    Documentation				
-				    </div>
-				    <div class='submenu_content'>
-				        <jdoc:include type="modules" name="left" />
-				    </div> 
-			    </div>
-	        </div>
-    	    
-	        <div id='main_content'>
-				<jdoc:include type="modules" name="right" />
+	        <?php if( $this->countModules( 'right' ) ) { ?>
+			<div id='main_content_half'>
+				<jdoc:include type="component" />
 			</div>
+			<div id='main_content_half'>
+		        	<jdoc:include type="modules" name="right" style="custom" />
+			</div>
+		<?php } else { ?>
+	        	<div id='main_content_wide'>
+				<jdoc:include type="component" />
+			</div>
+		<?php } ?>
 	        <div id='main_clear'></div>
     	</div>
+		<div id='bottomborder'>&#xA0;</div>
 	  
 		<div id='footer'>
-			(C) 2009 Johannes Passing
+			Visual Assert &#x2013; The Unit Testing Add-In for Visual C++<br />
+			<a href='contact.html'>Contact</a> |
+			<a href='terms.html'>Terms of Use</a><br /><br />
+			
+			(C) 2009 <a href='http://jpassing.com/'>Johannes Passing</a>. All rights reserved.
 		</div>
+	<script type="text/javascript">
+		var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+		document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+	</script>
+	<script type="text/javascript">
+		try {
+		var pageTracker = _gat._getTracker("UA-9197378-3");
+		pageTracker._trackPageview();
+		} catch(err) {}
+	</script>
     </body>
 </html>
