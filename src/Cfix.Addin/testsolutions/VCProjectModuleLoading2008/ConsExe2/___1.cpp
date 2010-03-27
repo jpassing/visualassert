41a2a0019e7f4e27ae5d96b4dcc1c0670ba2844b
(no author) <(no author)@92f0537e-14c0-1d44-b2ba-3b60a4d6dcee>
2010-03-27 08:42:16 +0000

    Test project files

---
+++ src/Cfix.Addin/testsolutions/VCProjectModuleLoading2008/ConsExe2/___1.cpp
@@ -0,0 +1,17 @@
+#include "StdAfx.h"
+#include <cfixcc.h>
+class ___1 : public cfixcc::TestFixture
+{
+private:
+
+public:
+	void Test()
+	{
+		CFIX_INCONCLUSIVE(__TEXT("Not implemented"));
+	}
+};
+
+CFIXCC_BEGIN_CLASS(___1)
+//	CFIXCC_METHOD(Test)
+CFIXCC_END_CLASS()
+
